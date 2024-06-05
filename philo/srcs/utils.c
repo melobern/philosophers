6855b14f506afa929a065eed81e55eabb07de6f8
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-05 11:55:48 +0200

    ft_atol_int is ok, error handling ok for overflows or negative numbers

--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "philo.h"
+
 static int	ft_skip_white_spaces(const char *str)
 {
 	int	x;
@@ -20,33 +22,79 @@ static int	ft_skip_white_spaces(const char *str)
 	return (x);
 }
 
-static int	ft_sign(char c)
+int	ft_atol_int(const char *str)
 {
-	if (c == '-')
-		return (-1);
-	else
-		return (1);
-}
-
-int	ft_atoi(const char *str)
-{
-	int	i;
-	int	sign;
-	int	nb;
+	long	i;
+	long	nb;
 
 	nb = 0;
 	i = ft_skip_white_spaces(str);
-	sign = ft_sign(str[i]);
-	if (ft_strncmp(str + i, "-2147483648", 12) == 0)
-		return (-2147483648);
-	if (str[i] == '+' || str[i] == '-')
+	if (str[i] && (str[i] == '+' || str[i] == '-'))
 		i++;
-	while (ft_isdigit(str[i]))
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		if ((nb * 10 + (str[i] - '0')) < nb)
-			return (-(sign == 1));
 		nb = nb * 10 + (str[i] - '0');
 		i++;
+		if (nb > INT_MAX || nb < INT_MIN)
+			return (-2);
 	}
-	return ((nb * sign));
+	if (str[0] && str[0] == '-')
+		return (-1);
+	return ((int)nb);
 }
+
+//
+//void	clear_data(t_data	*data)
+//{
+//	if (data->tid)
+//		free(data->tid);
+//	if (data->forks)
+//		free(data->forks);
+//	if (data->philos)
+//		free(data->philos);
+//}
+//
+//
+//void	ft_exit(t_data *data)
+//{
+//	int	i;
+//
+//	i = -1;
+//	while (++i < data->philo_num)
+//	{
+//		pthread_mutex_destroy(&data->forks[i]);
+//		pthread_mutex_destroy(&data->philos[i].lock);
+//	}
+//	pthread_mutex_destroy(&data->write);
+//	pthread_mutex_destroy(&data->lock);
+//	clear_data(data);
+//}
+//
+//
+//int	error(char *str, t_data *data)
+//{
+//	printf("%s\n", str);
+//	if (data)
+//		ft_exit(data);
+//	return (1);
+//}
+//
+//u_int64_t	get_time(void)
+//{
+//	struct timeval	tv;
+//
+//	if (gettimeofday(&tv, NULL))
+//		return (error("gettimeofday() FAILURE\n", NULL));
+//	return ((tv.tv_sec * (u_int64_t)1000) + (tv.tv_usec / 1000));
+//}
+//
+//
+//int	ft_usleep(useconds_t time)
+//{
+//	u_int64_t	start;
+//
+//	start = get_time();
+//	while ((get_time() - start) < time)
+//		usleep(time / 10);
+//	return(0);
+//}
