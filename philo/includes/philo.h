17a8e7df303464ca051e2d1a03b91d51a04e4bb8
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-06-05 11:55:48 +0200

    ft_atol_int is ok, error handling ok for overflows or negative numbers

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -18,18 +18,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <limits.h>
 #include <pthread.h>
 #include <sys/time.h>
+#include <sys/types.h>
 
 typedef	struct s_philo_table
 {
+	bool	table_assigned;
+	bool	meals_defined;
 	int	num_of_philos;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
-	int	num_must_eat;
+	int	number_of_meals;
 }		t_philo_table;
 
 //////////////////////				UTILS 			////////////////////////////
-int	ft_atoi(const char *str);
+int	ft_atol_int(const char *str);
+//int	ft_usleep(useconds_t time);
+//u_int64_t	get_time(void);
+
 #endif // PHILO_H
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -19,9 +19,8 @@ bool check_overflows(char **av)
 	i = 1;
 	while (av[i])
 	{
-		if (atoi(av[i]) < 0)
+		if (ft_atol_int(av[i]) < 0)
 		{
-			write(2, "Error: overflow in arguments\n", 29);
 			return (1);
 		}
 		i++;
@@ -29,27 +28,67 @@ bool check_overflows(char **av)
 	return (0);
 }
 
-void	assign_arguments_to_philo_table(t_philo_table *philo_table, char **av)
+void	assign_arguments_to_philo_table(t_philo_table *philo_table, int value, int var)
 {
-	if (check_overflows == 1)
-		return ;
-	else
+	if (var == 1)
+		philo_table->num_of_philos = value;
+	else if (var == 2)
+		philo_table->time_to_die = value;
+	else if (var == 3)
+		philo_table->time_to_eat = value;
+	else if (var == 4)
+		philo_table->time_to_sleep = value;
+	else if (var == 5)
 	{
-		philo_table->num_of_philos = ft_atoi(av[1]);
-		philo_table->time_to_die = ft_atoi(av[2]);
-		philo_table->time_to_eat = ft_atoi(av[3]);
-		philo_table->time_to_sleep = ft_atoi(av[4]);
-		philo_table->num_must_eat = ft_atoi(av[5]);
-
+		philo_table->number_of_meals = value;
+		philo_table->meals_defined = true;
 	}
+}
+
+void check_arguments_and_assign(char **av, t_philo_table *philo_table)
+{
+	int i;
+	int atol_value;
+
+	i = 1;
+	philo_table->table_assigned = false;
+	philo_table->meals_defined = false;
+	while (av[i])
+	{
+		atol_value = ft_atol_int(av[i]);
+		if (atol_value == -1)
+		{
+			write(2, "Error: arguments must be positive integers\n", 43);
+			return ;
+		}
+		else if (atol_value == -2)
+		{
+			write(2, "Error: overflow in arguments\n", 29);
+			return ;
+		}
+		assign_arguments_to_philo_table(philo_table, atol_value, i);
+		i++;
+	}
+	philo_table->table_assigned = true;
 }
 
 int	main(int ac, char **av)
 {
 	t_philo_table philo_table;
 
-	if (ac != 6)
+	if (ac < 5 || ac > 6)
+	{
 		write(2, "Error: wrong number of arguments\n", 33);
-	else
-		assign_arguments_to_philo_table(&philo_table, av);
+		return (1);
+	}
+	check_arguments_and_assign(av, &philo_table);
+	if (!philo_table.table_assigned)
+		return (1);
+	printf("num_of_philos: %d\n", philo_table.num_of_philos);
+	printf("time_to_die: %d\n", philo_table.time_to_die);
+	printf("time_to_eat: %d\n", philo_table.time_to_eat);
+	printf("time_to_sleep: %d\n", philo_table.time_to_sleep);
+	if (philo_table.meals_defined)
+		printf("number_of_meals: %d\n", philo_table.number_of_meals);
+	return (0);
 }
