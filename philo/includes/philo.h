fb64eae9ee6106c9a2c03b79722eb61a81703fc0
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-05 13:41:12 +0200

    tidied files, better verification with static functions in one file and a little norminette

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -13,30 +13,33 @@
 #ifndef PHILO_H
 # define PHILO_H
 
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <limits.h>
-#include <pthread.h>
-#include <sys/time.h>
-#include <sys/types.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <sys/types.h>
+# include <unistd.h>
 
-typedef	struct s_philo_table
+typedef struct s_philo_table
 {
 	bool	table_assigned;
 	bool	meals_defined;
-	int	num_of_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_meals;
-}		t_philo_table;
+	int		num_of_philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		number_of_meals;
+}			t_philo_table;
 
 //////////////////////				UTILS 			////////////////////////////
-int	ft_atol_int(const char *str);
-//int	ft_usleep(useconds_t time);
-//u_int64_t	get_time(void);
+//////////////////////		CHECK AND ASSIGN 		////////////////////////////
+void		check_arguments_and_assign(char **av, t_philo_table *philo_table);
+
+// int	ft_atol_int(const char *str);
+// int	ft_usleep(useconds_t time);
+// u_int64_t	get_time(void);
 
 #endif // PHILO_H
