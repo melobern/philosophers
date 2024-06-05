753f48c5f2e2559306ae60a0247cbc22cb8dfe4e
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-06-05 16:20:12 +0200

    check zeros should be ok

--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -11,34 +11,34 @@
 /* ************************************************************************** */
 
 #include "philo.h"
+//
+//static void destroy_philo_table(t_philo_table *philo_table)
+//{
+//	// destroy_threads(philo_table);
+//	if (philo_table->philos)
+//		free(philo_table->philos);
+//}
 
-static void destroy_philo_table(t_philo_table *philo_table)
-{
-	// destroy_threads(philo_table);
-	if (philo_table->philos)
-		free(philo_table->philos);
-}
-
-static bool init_philo_table(t_philo_table *philo_table)
-{
-	philo_table->philos = malloc(sizeof(t_philo_thread) * philo_table->num_of_philos);
-	if (!philo_table->philos)
-	{
-		write(2, "Error: malloc failed\n", 22);
-		return (false);
-	}
-	for (int i = 0; i < philo_table->num_of_philos; i++)
-	{
-		philo_table->philos[i].id = i + 1;
-		if (pthread_create(&philo_table->philos[i].thread, NULL, &philo_routine, &philo_table->philos[i]))
-		{
-			write(2, "Error: pthread_create failed\n", 30);
-			destroy_philo_table(philo_table);
-			return (false);
-		}
-	}
-	return (true);
-}
+//static bool init_philo_table(t_philo_table *philo_table)
+//{
+//	philo_table->philos = malloc(sizeof(t_philo_thread) * philo_table->num_of_philos);
+//	if (!philo_table->philos)
+//	{
+//		write(2, "Error: malloc failed\n", 22);
+//		return (false);
+//	}
+//	for (int i = 0; i < philo_table->num_of_philos; i++)
+//	{
+//		philo_table->philos[i].id = i + 1;
+//		if (pthread_create(&philo_table->philos[i].thread, NULL, &philo_routine, &philo_table->philos[i]))
+//		{
+//			write(2, "Error: pthread_create failed\n", 30);
+//			destroy_philo_table(philo_table);
+//			return (false);
+//		}
+//	}
+//	return (true);
+//}
 
 int	main(int ac, char **av)
 {
@@ -58,8 +58,8 @@ int	main(int ac, char **av)
 	printf("time_to_sleep: %d\n", philo_table.time_to_sleep);
 	if (philo_table.meals_defined)
 		printf("number_of_meals: %d\n", philo_table.number_of_meals);
-	if (init_philo_table(&philo_table) == false)
-		return (1);
+//	if (init_philo_table(&philo_table) == false)
+//		return (1);
 //	init_mutex(&philo_table);
 //	launch_philo_threads(&philo_table);
 	return (0);
