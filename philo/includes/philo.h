6f701cfa0a1c61f66c6d66d93470ac463d7fcb9a
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-06 11:06:50 +0200

    good initiation of the philosophers

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 11:56:09 by mbernard          #+#    #+#             */
-/*   Updated: 2024/06/04 13:30:42 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/06/06 11:00:57 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,14 @@
 # ifndef PHILO_200_ERROR
 #  define PHILO_200_ERROR \
 	"Error: number of philosophers should be inferior to 200\n"
+# endif
+
+# ifndef WRONG_NUMBER_OF_ARGUMENTS
+#  define WRONG_NUMBER_OF_ARGUMENTS \
+	"Error: wrong number of arguments\n\
+Correct format should contains 5 or 6 arguments :\n\
+number_of_philosophers	time_to_die	time_to_eat	time_to_sleep\t\
+[number_of_times_each_philosopher_must_eat]\n"
 # endif
 
 # ifndef FORK
@@ -52,6 +60,12 @@ typedef struct s_philo_thread
 {
 	pthread_t		thread;
 	int				id;
+	bool			is_dead;
+	bool			is_eating;
+	int				meals_eaten;
+	u_int64_t		last_meal;
+//	bool			lock;
+	pthread_mutex_t	mutex;
 }					t_philo_thread;
 
 typedef struct s_philo_table
@@ -71,7 +85,7 @@ void				check_arguments_and_assign(char **av,
 						t_philo_table *philo_table);
 //////////////////////		UTILS						////////////////////////
 int					ft_usleep(useconds_t time);
-u_int64_t			get_time(void);
+u_int64_t			get_time_in_ms(void);
 //////////////////////		ROUTINE 				////////////////////////////
 void				*routine(void *arg);
 
--- philo/srcs/philo.c
+++ philo/srcs/philo.c
@@ -6,39 +6,60 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 12:02:33 by mbernard          #+#    #+#             */
-/*   Updated: 2024/06/04 13:28:55 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/06/06 11:03:03 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-//static void destroy_philo_table(t_philo_table *philo_table)
-//{
-//	// destroy_threads(philo_table);
-//	if (philo_table->philos)
-//		free(philo_table->philos);
-//}
-//
-//static bool init_philo_table(t_philo_table *philo_table)
-//{
-//	philo_table->philos = malloc(sizeof(t_philo_thread) * philo_table->num_of_philos);
-//	if (!philo_table->philos)
-//	{
-//		write(2, "Error: malloc failed\n", 22);
-//		return (false);
-//	}
-//	for (int i = 0; i < philo_table->num_of_philos; i++)
-//	{
-//		philo_table->philos[i].id = i + 1;
-//		if (pthread_create(&philo_table->philos[i].thread, NULL, &routine, &philo_table->philos[i]))
-//		{
-//			write(2, "Error: pthread_create failed\n", 30);
-//			destroy_philo_table(philo_table);
-//			return (false);
-//		}
-//	}
-//	return (true);
-//}
+static void	destroy_philo_table(t_philo_table *philo_table)
+{
+	int i;
+
+	i = 0;
+	while (i < philo_table->num_of_philos)
+	{
+		pthread_join(philo_table->philos[i].thread, NULL);
+		pthread_mutex_destroy(&philo_table->philos[i].mutex);
+		i++;
+	}
+	// destroy_threads(philo_table);
+	if (philo_table->philos)
+		free(philo_table->philos);
+}
+
+static bool	init_philo_table(t_philo_table *philo_table)
+{
+	int	i;
+
+	i = 0;
+	philo_table->philos = malloc(sizeof(t_philo_thread) * philo_table->num_of_philos);
+	if (!philo_table->philos)
+	{
+		write(2, "Error: malloc failed at init_philo_table\n", 41);
+		return (false);
+	}
+	while (i < philo_table->num_of_philos)
+	{
+		philo_table->philos[i].id = i;
+		philo_table->philos[i].is_dead = false;
+		philo_table->philos[i].is_eating = false;
+		philo_table->philos[i].meals_eaten = 0;
+		philo_table->philos[i].last_meal = get_time_in_ms();
+		i++;
+	}
+	return (true);
+}
+
+static void	Print_to_erase(t_philo_table *philo_table)
+{
+	printf("num_of_philos: %d\n", philo_table->num_of_philos);
+	printf("time_to_die: %d\n", philo_table->time_to_die);
+	printf("time_to_eat: %d\n", philo_table->time_to_eat);
+	printf("time_to_sleep: %d\n", philo_table->time_to_sleep);
+	if (philo_table->meals_defined)
+		printf("number_of_meals: %d\n", philo_table->number_of_meals);
+}
 
 int	main(int ac, char **av)
 {
@@ -46,22 +67,27 @@ int	main(int ac, char **av)
 
 	if (ac < 5 || ac > 6)
 	{
-		write(2, "Error: wrong number of arguments\n", 33);
+		write(2, WRONG_NUMBER_OF_ARGUMENTS, 188);
 		return (1);
 	}
 	check_arguments_and_assign(av, &philo_table);
 	if (!philo_table.table_assigned)
 		return (1);
-	printf("num_of_philos: %d\n", philo_table.num_of_philos);
-	printf("time_to_die: %d\n", philo_table.time_to_die);
-	printf("time_to_eat: %d\n", philo_table.time_to_eat);
-	printf("time_to_sleep: %d\n", philo_table.time_to_sleep);
-	if (philo_table.meals_defined)
-		printf("number_of_meals: %d\n", philo_table.number_of_meals);
-//	if (init_philo_table(&philo_table) == false)
-//		return (1);
+	Print_to_erase(&philo_table);
+	if (init_philo_table(&philo_table) == false)
+		return (1);
 //	init_mutex(&philo_table);
 //	launch_philo_threads(&philo_table);
+/*
+ * 		if (pthread_create(&philo_table->philos[i].thread, NULL, &routine, &philo_table->philos[i]))
+		{
+			write(2, "Error: pthread_create failed\n", 30);
+			destroy_philo_table(philo_table);
+			return (false);
+		}
+		pthread_mutex_init(&philo_table->philos[i].mutex, NULL);
+*/
+ */
 //	destroy_philo_table(&philo_table);
 	return (0);
 }
