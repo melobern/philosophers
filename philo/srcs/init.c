77561ca97d42c4ca14ce09677db8cc6d9eb7d3b4
melodie <melodie.bernard123@yahoo.fr>
2024-06-06 19:15:35 +0200

    init.c better

--- philo/srcs/init.c
+++ philo/srcs/init.c
@@ -6,13 +6,13 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:57:26 by mbernard          #+#    #+#             */
-/*   Updated: 2024/06/06 18:57:26 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/06/06 19:10:13 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-static void	assign_table(const t_philo_table *t, int i, unsigned int time)
+static void	assign_table(t_philo_table *t, int i, unsigned int time)
 {
 	t->philos[i].id = i + 1;
 	t->philos[i].is_eating = false;
@@ -20,15 +20,17 @@ static void	assign_table(const t_philo_table *t, int i, unsigned int time)
 	t->philos[i].last_meal = time;
 	t->philos[i].is_dead = &(t->a_philo_has_died);
 	t->philos[i].write_mutex = &(t->write_mutex);
-	if (i < t->num_of_philos - 1)
-		t->philos[i].right_fork = &(t->philos[i + 1].left_fork);
-	else
-		t->philos[i].right_fork = &(t->philos[0].left_fork);
-	if (i < t->num_of_philos - 1)
-		t->philos[i].right_fork_taken = &(t->philos[i + 1].left_fork_taken);
-	else
-		t->philos[i].right_fork_taken = &(t->philos[0].left_fork_taken);
 	t->philos[i].dinner_started = &(t->dinner_started);
+	if (i < t->num_of_philos - 1)
+	{
+		t->philos[i].right_fork = &(t->philos[i + 1].left_fork);
+		t->philos[i].right_fork_taken = &(t->philos[i + 1].left_fork_taken);
+	}
+	else
+	{
+		t->philos[i].right_fork = &(t->philos[0].left_fork);
+		t->philos[i].right_fork_taken = &(t->philos[0].left_fork_taken);
+	}
 }
 
 bool	init_philo_table(t_philo_table *table)
@@ -48,7 +50,6 @@ bool	init_philo_table(t_philo_table *table)
 	while (i < table->num_of_philos)
 	{
 		assign_table(table, i, time);
-		printf("philo id: %d\n", table->philos[i].id);
 		i++;
 	}
 	return (true);
