3a107b02fcb8191dfa7d305845a77bfd40374a98
Melodie Bernard (from school) <melodie.bernard123@yahoo.fr>
2024-06-14 09:33:16 +0200

    better increasing of meals value

--- philo/srcs/mutex.c
+++ philo/srcs/mutex.c
@@ -12,45 +12,18 @@
 
 #include "philo.h"
 
-bool	mutex_lock(pthread_mutex_t *mutex)
-{
-	if (pthread_mutex_lock(mutex))
-	{
-		write(2, "Error: mutex lock failed\n", 25);
-		return (false);
-	}
-	return (true);
-}
-
-bool	mutex_unlock(pthread_mutex_t *mutex)
-{
-	if (pthread_mutex_unlock(mutex))
-	{
-		write(2, "Error: mutex unlock failed\n", 27);
-		return (false);
-	}
-	return (true);
-}
-
-int	assign_bool_mutex(bool *var, pthread_mutex_t *mutex, bool value)
+bool	assign_bool_mutex(bool *var, pthread_mutex_t *m, bool value)
 {
 	bool flag;
 
 	flag = true;
-	if (pthread_mutex_lock(mutex))
-	{
-		write(2, "Error: mutex lock failed\n", 25);
-		return (-1);
-	}
+
+	pthread_mutex_lock(m);
 	if (*var != value)
 		*var = value;
 	else
 		flag = false;
-	if (pthread_mutex_unlock(mutex))
-	{
-		write(2, "Error: mutex unlock failed\n", 27);
-		return (-1);
-	}
+	pthread_mutex_unlock(m);
 	return (flag);
 }
 
@@ -59,22 +32,14 @@ bool	mutex_check_if_can_eat(t_philo_thread *p)
 	bool flag;
 
 	flag = false;
-//	if (p->is_eating == false
-//		&& (p->meals_defined == false || p->meals_eaten <= p->meals_num))
-if (p->meals_defined == false || p->meals_eaten <= p->meals_num)
-{
-		if (pthread_mutex_lock(p->death_mutex))
-		{
-			write(2, "Error: mutex lock failed\n", 25);
-			return (false);
+	//	if (p->is_eating == false
+	//		&& (p->meals_defined == false || p->meals_eaten <= p->meals_num))
+	if (p->meals_defined == false || p->meals_eaten < p->meals_num)
+	{
+			pthread_mutex_lock(p->death_mutex);
+			if (*(p->dead_detected) == false)
+				flag = true;
+			pthread_mutex_unlock(p->death_mutex);
 		}
-		if (*(p->dead_detected) == false)
-			flag = true;
-		if (pthread_mutex_unlock(p->death_mutex))
-		{
-			write(2, "Error: mutex lock failed\n", 25);
-			return (false);
-		}
-	}
 	return (flag);
 }
